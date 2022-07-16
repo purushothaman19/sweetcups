ff4eb7a14d1f969a5eb77e924edf357e475efb5b
Purushothaman Ganesan <71747442+purushothaman19@users.noreply.github.com>
2022-07-17 00:01:26 +0530

    wadf

--- src/components/Admin/Add.jsx
+++ src/components/Admin/Add.jsx
@@ -115,7 +115,7 @@ export default function AddItem(props) {
         axios({
             method: 'post',
             url: 'http://localhost:4000/dashboardAdd',
-            data: {"cakeName":cakeName,"prices":prices,"cakeImgUrl":img}
+            data: {"cakeName":cakeName,"prices":prices,"cakeImgUrl":cake.cakeImgUrl}
    
           }).then( res => console.log(res.data))
     .catch( err => console.log(err));
