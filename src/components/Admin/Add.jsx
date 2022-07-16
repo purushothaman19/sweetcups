b1fd3ac07dc5664b12bf4684dab11355d5513084
Purushothaman Ganesan <71747442+purushothaman19@users.noreply.github.com>
2022-07-16 23:51:27 +0530

    fdsg

--- src/components/Admin/Add.jsx
+++ src/components/Admin/Add.jsx
@@ -115,7 +115,7 @@ export default function AddItem(props) {
         axios({
             method: 'post',
             url: 'http://localhost:4000/dashboardAdd',
-            data: {"cake":prices},
+            data: {"cakeName":cakeName,"prices":prices,"cakeImgUrl":img}
    
           }).then( res => console.log(res.data))
     .catch( err => console.log(err));
